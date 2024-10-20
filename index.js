b141588d44304e396d9b6e95dbd41868b48d1d28
Artem Smotrytel <artemsmotritel@gmail.com>
2024-10-20 14:32:03 +0300

    add basic validation

--- index.js
+++ index.js
@@ -5,8 +5,42 @@ const alphabetInput = document.getElementById('alphabet-input');
 const cipherTextInput = document.getElementById('cipher-text');
 const decipherTextInput = document.getElementById('decipher-text');
 
+const resetInputs = () => {
+    const inputs = [cipherMethodInput, cipherKeyInput, alphabetInput, cipherTextInput, decipherTextInput];
+    const ariaValid = input => input.ariaInvalid = "";
+    const helperEmpty = input => {
+        const helper = document.getElementById(input.id + "-helper");
+        if (!helper) {
+            return;
+        }
+        helper.innerText = "";
+    };
+
+    inputs.forEach(input => {
+        ariaValid(input);
+        helperEmpty(input);
+    })
+};
+
+function isValidPositiveInteger(str) {
+    if (typeof str !== 'string' || str.trim() === '') {
+        return false;
+    }
+    return /^[1-9]\d*$/.test(str);
+}
+
+
+const validateInputs = (method, alphabet, cipherKey, text) => {
+    if (!isValidPositiveInteger(cipherKey)) {
+        cipherKeyInput.ariaInvalid = 'true';
+        document.getElementById(cipherKeyInput.id + "-helper").innerText = 'Cipher key is not a positive integer';
+        throw new Error('validation failed')
+    }
+}
+
 const handleCipher = (e) => {
     e.preventDefault();
+    resetInputs();
     console.log('ciphering');
 
     try {
@@ -14,6 +48,7 @@ const handleCipher = (e) => {
         const cipherKey = cipherKeyInput.value;
         const alphabet = alphabetInput.value;
         const text = cipherTextInput.value;
+        validateInputs(method, alphabet, cipherKey, text);
 
         decipherTextInput.value = cipher(method, alphabet, cipherKey, text);
     } catch (e) {
@@ -24,12 +59,14 @@ const handleCipher = (e) => {
 
 const handleDecipher = (e) => {
     e.preventDefault();
+    resetInputs();
     console.log('deciphering');
     try {
         const method = cipherMethodInput.value;
         const cipherKey = cipherKeyInput.value;
         const alphabet = alphabetInput.value;
         const text = decipherTextInput.value;
+        validateInputs(method, alphabet, cipherKey, text);
 
         cipherTextInput.value = decipher(method, alphabet, cipherKey, text);
     } catch (e) {
